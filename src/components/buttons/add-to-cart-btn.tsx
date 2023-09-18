f0abae7b901464cd1ceb562725874100ede7c5e3
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2023-09-18 20:52:27 +0200

    fix: :bug: fix styles in add-to-cart btn

--- src/components/buttons/add-to-cart-btn.tsx
+++ src/components/buttons/add-to-cart-btn.tsx
@@ -27,7 +27,6 @@ const AddToCartButton: React.FC<AddToCartButtonProps> = ({
       onClick={handleAddToCart}
       startIcon={<ShoppingCartIcon />}
       disabled={isInCart || isLoadingButton}
-      style={{ marginTop: "10px" }}
     >
       {isLoadingButton ? (
         <CircularProgress size={20} color="inherit" />
